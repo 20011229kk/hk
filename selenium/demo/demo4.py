c56f2dfcb81bcf5c7f4a4d06a2e76d15202d81dc
20011229kk <2939503115@qq.com>
2025-02-18 14:10:45 +0800

    Delete selenium directory

--- selenium/demo/demo4.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-dirver = webdriver.Chrome()
-dirver.get("https://www.baidu.com")
-
-element = dirver.find_element(By.LINK_TEXT, "hao123")
-print(element.text)
-dirver.quit()
